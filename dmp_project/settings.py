a71d0f0e36185fbfbc62426e9cbf4be387b9cbbb
mercadeomarmaz-ops <mercadeo.marmaz@gmail.com>
2026-04-03 21:04:09 -0500

    Update settings.py

--- dmp_project/settings.py
+++ dmp_project/settings.py
@@ -135,7 +135,7 @@ SECURE_SSL_REDIRECT = not DEBUG
 PUBLISHER_NAME = os.getenv("PUBLISHER", "DMP - FREE MUSIC CATALOGUE SOFTWARE")
 
 # CWR Delivery code, issued by collecting societies
-PUBLISHER_CODE = os.getenv("PUBLISHER_CODE", "000")
+PUBLISHER_CODE = os.getenv("PUBLISHER_CODE", "00072279219")
 
 # IPI Name # is required, issued by collecting societies
 PUBLISHER_IPI_NAME = os.getenv("PUBLISHER_IPI_NAME", "")
